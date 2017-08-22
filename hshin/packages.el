f5b25b02aed6d05970de00f1799742f03b8c5dba
Shin Heejae <hshin@gracenote.com>
2017-08-22 17:21:03 +0900

    mi-team style is modified

--- hshin/packages.el
+++ hshin/packages.el
@@ -66,19 +66,73 @@ Each entry is either:
                  '("google"
                    (c-basic-offset . 4)     ; Guessed value
                    (c-offsets-alist
-                    (access-label . 0)      ; Guessed value
                     (block-close . 0)       ; Guessed value
-                    (class-close . 0)       ; Guessed value
+                    (catch-clause . 0)      ; Guessed value
                     (defun-block-intro . +) ; Guessed value
-                    (inclass . +)           ; Guessed value
-                    (inline-close . 0)      ; Guessed value
-                    (innamespace . 0)       ; Guessed value
-                    (member-init-intro . +)     ; Guessed value
-                    (namespace-close . 0)   ; Guessed value
+                    (defun-close . 0)       ; Guessed value
                     (statement . 0)         ; Guessed value
                     (statement-block-intro . +) ; Guessed value
-                    (topmost-intro . +)         ; Guessed value
-                    (topmost-intro-cont . 0)))) ; Guessed value
+                    (topmost-intro . 0)         ; Guessed value
+                    (annotation-top-cont . 0)
+                    (annotation-var-cont . +)
+                    (arglist-close . c-lineup-close-paren)
+                    (arglist-cont c-lineup-gcc-asm-reg 0)
+                    (arglist-cont-nonempty . c-lineup-arglist)
+                    (arglist-intro google-c-lineup-expression-plus-4)
+                    (block-open . 0)
+                    (brace-entry-open . 0)
+                    (brace-list-close . 0)
+                    (brace-list-entry . 0)
+                    (brace-list-intro . +)
+                    (brace-list-open . 0)
+                    (c . c-lineup-C-comments)
+                    (case-label . +)
+                    (catch-clause . 0)
+                    (class-open . 0)
+                    (comment-intro . c-lineup-comment)
+                    (composition-close . 0)
+                    (composition-open . 0)
+                    (cpp-define-intro c-lineup-cpp-define +)
+                    (cpp-macro . -1000)
+                    (cpp-macro-cont . +)
+                    (defun-close . 0)
+                    (defun-open . 0)
+                    (do-while-closure . 0)
+                    (else-clause . 0)
+                    (extern-lang-close . 0)
+                    (extern-lang-open . 0)
+                    (friend . 0)
+                    (func-decl-cont . ++)
+                    (incomposition . +)
+                    (inexpr-class . +)
+                    (inexpr-statement . +)
+                    (inextern-lang . +)
+                    (inher-cont . c-lineup-multi-inher)
+                    (inher-intro . ++)
+                    (inlambda . c-lineup-inexpr-block)
+                    (inline-open . 0)
+                    (inmodule . +)
+                    (knr-argdecl . 0)
+                    (knr-argdecl-intro . +)
+                    (label . /)
+                    (lambda-intro-cont . +)
+                    (member-init-cont . c-lineup-multi-inher)
+                    (module-close . 0)
+                    (module-open . 0)
+                    (namespace-open . 0)
+                    (objc-method-args-cont . c-lineup-ObjC-method-args)
+                    (objc-method-call-cont c-lineup-ObjC-method-call-colons c-lineup-ObjC-method-call +)
+                    (objc-method-intro .
+                                       [0])
+                    (statement-case-intro . +)
+                    (statement-case-open . +)
+                    (statement-cont nil nil ++)
+                    (stream-op . c-lineup-streamop)
+                    (string . -1000)
+                    (substatement . +)
+                    (substatement-label . 2)
+                    (substatement-open . 0)
+                    (template-args-cont c-lineup-template-args +)))) 
 
     (defun hshin-c++-mode-hook ()
       (c-set-style "mi-team"))        ; use my-style defined above
